cc561cb3d86ebd10bff2da82906d7bae42724627
Phil <phil_dootson1@hotmail.com>
2017-07-29 13:52:56 +0100

    Uploaded FizzBuzz kata

---
+++ Katas/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static void Run()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(ProduceStringFromInt(i));
+            }
+        }
+
+
+        public static string ProduceStringFromInt(int i)
+        {
+            if (i % 15 == 0)
+                return "FizzBuzz";
+
+            if (i % 3 == 0)
+                return "Fizz";
+
+            if (i % 5 == 0)
+                return "Buzz";
+
+            return i.ToString();
+        }
+    }
+}
---
+++ Katas/FizzBuzz/UnitTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(4, "4")]
+        [TestCase(7, "7")]
+        [TestCase(8, "8")]
+        public void ShouldReturnStringOfPassedIntValueWhenNotDivisbleByThreeOrFive(int input, string output)
+        {
+            Assert.AreEqual(output, FizzBuzz.ProduceStringFromInt(input));
+        }
+
+
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        [TestCase(12)]
+        [TestCase(18)]
+        public void ShouldReturnStringFizzWhenIntValueOnlyDivisibleByThree(int input)
+        {
+            Assert.AreEqual("Fizz", FizzBuzz.ProduceStringFromInt(input));
+        }
+
+
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        [TestCase(25)]
+        public void ShouldReturnStringBuzzWhenIntValueOnlyDivisibleByFive(int input)
+        {
+            Assert.AreEqual("Buzz", FizzBuzz.ProduceStringFromInt(input));
+        }
+
+
+        [TestCase(15)]
+        [TestCase(30)]
+        [TestCase(45)]
+        public void ShouldReturnStringFizzBuzzWhenIntValueDivisibleByThreeAndFive(int input)
+        {
+            Assert.AreEqual("FizzBuzz", FizzBuzz.ProduceStringFromInt(input));
+        }
+    }
+}
