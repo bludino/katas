5d9a374684769d62276e5366876dfc045204faf6
Phil <phil_dootson1@hotmail.com>
2017-08-08 19:47:35 +0100

    First attempt at StringCalculator kata

---
+++ Katas/StringCalculator/StringCalc.cs
@@ -0,0 +1,35 @@
+﻿namespace StringCalculator
+{
+    public class StringCalc
+    {
+        public int Add(string numbers)
+        {
+            int result = 0;
+
+            if (string.IsNullOrEmpty(numbers))
+                return result;
+
+            string[] array;
+
+            if (numbers.StartsWith("//"))
+            {
+                char delimiter = char.Parse(numbers.Substring(2, 1));
+                numbers = numbers.Substring(numbers.IndexOf('\n') + 1);
+
+                array = numbers.Split(delimiter);
+            }
+            else
+            {
+                array = numbers.Split(new char[] { ',', '\n' });
+            }
+
+
+            foreach (string item in array)
+            {
+                result += int.Parse(item);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Katas/StringCalculator/UnitTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        private readonly StringCalc _stringCalc = new StringCalc();
+
+        [Test]
+        public void Add_EmptyStringReturnsZero()
+        {
+            Assert.AreEqual(0, _stringCalc.Add(string.Empty));
+        }
+
+
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        public void Add_SingleValuePassedReturnedAsInt(int expected, string passedString)
+        {
+            Assert.AreEqual(expected, _stringCalc.Add(passedString));
+        }
+
+
+        [TestCase(3, "1,2")]
+        [TestCase(5, "3,2")]
+        [TestCase(6, "1,2,3")]
+        public void Add_SumValuesOfPassedCommaSeparatedString(int expected, string passedString)
+        {
+            Assert.AreEqual(expected, _stringCalc.Add(passedString));
+        }
+
+
+        [TestCase(6, "1\n2,3")]
+        [TestCase(6, "1,2\n3")]
+        [TestCase(6, "1\n2\n3")]
+        public void Add_ShouldBeAbleToHandleNewLinesBetweenEntries(int expected, string passedString)
+        {
+            Assert.AreEqual(expected, _stringCalc.Add(passedString));
+        }
+
+
+        [Test]
+        public void Add_CalculatorShouldHandleAlternativeDelimiters()
+        {
+            Assert.AreEqual(3, _stringCalc.Add("//;\n1;2"));
+        }
+    }
+}
